4795ab42a7aad0a0bb42fcf39cb7ea3688c60711
MikeDre <info@mikedre.com>
2022-12-14 18:32:27 +0000

    Added: base hooks for useState, created custom debouncer hook to handle search limiting

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import './styles/_main.scss'
+import { useState, useEffect } from 'react';
+import useDebounce from './hooks/use-debounce';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isSearching, setisSearching] = useState(false);
+
+  const handleAPIQuery = (query: string) => {
+    // Handle search query here...
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TaxScouts</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <input type="text" value={query} onChange={e => handleAPIQuery(e.target.value)} placeholder="Enter a book title..."/>
+
+        <div className="books">
+        </div>
     </div>
   )
 }
 
-export default App
+export default App;

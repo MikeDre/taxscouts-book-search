6f5d566546fe21e9fd19988651d3188b48db668c
MikeDre <info@mikedre.com>
2022-12-14 18:39:38 +0000

    Added: progress for functions to fetch books from api, Added: useEffect to handle the debounced query

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,45 @@ function App() {
 
   const [books, setBooks] = useState([]);
   const [query, setQuery] = useState('');
-  const [isSearching, setisSearching] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
 
-  const handleAPIQuery = (query: string) => {
-    // Handle search query here...
+  // Debounce the query so that we don't fetch new books on every keystroke
+  const debouncedQuery = useDebounce(query, 250);
+
+  useEffect(() => {
+    fetchBooks(query).then(books => {
+      setBooks(books);
+    });
+  }, [debouncedQuery]);
+
+  // Handle user submitted query for Open Library API
+  const handleAPIQuery = (submittedQuery: string) => {  
+    setQuery(submittedQuery);
   }
+
+  // Format query for API / Search engines
+  const formatQuery = (preformatedQuery: string) => {
+    const formattedQuery = preformatedQuery.replace(/\s/g, '+');
+  
+    return formattedQuery;
+  }
+
+  // Fetch books from the Open Library API
+  const fetchBooks = async (query: string) => {
+    let books = [];
+  
+    const formatedQuery = formatQuery(query);
+
+    const response = await fetch(`http://openlibrary.org/search.json?q=${formatedQuery}`);
+    const data = await response.json();
+
+    return books;
+  }
+
 
   return (
     <div className="App">
-        <input type="text" value={query} onChange={e => handleAPIQuery(e.target.value)} placeholder="Enter a book title..."/>
+        <input type="text" value={src/App.tsx} onChange={e => handleAPIQuery(e.target.value)} placeholder="Enter a book title..."/>
 
         <div className="books">
         </div>
